ef4690b5c4c6bb8c1dc1e1e36b1337e286cf6d22
Alex Kilgore <alex.kilgore00@gmail.com>
2025-01-21 20:51:51 -0600

    Added type effectiveness chart

--- src/types.py
+++ src/types.py
@@ -27,5 +27,182 @@ class Type(Enum):
         except KeyError:
             raise ValueError(f"Invalid type: {type_string}")
         
+    @classmethod
+    def get_effectiveness(cls, attacking_type: 'Type', defending_type: 'Type') -> float:
+        """
+        Get the effectiveness multiplier when attacking_type moves are used against defending_type.
+        Returns:
+        - 2.0 for super effective
+        - 1.0 for normal effectiveness
+        - 0.5 for not very effective
+        - 0.0 for no effect
+        """
+        try:
+            return cls.TYPE_CHART[attacking_type][defending_type]
+        except KeyError:
+            return 1.0  # default to normal effectiveness
+        
     def __str__(self):
         return self.name
+    
+
+
+
+Type.TYPE_CHART = {
+    Type.NORMAL: {
+        Type.ROCK: 0.5,
+        Type.STEEL: 0.5,
+        Type.GHOST: 0.0,
+    },
+    Type.FIRE: {
+        Type.GRASS: 2.0,
+        Type.ICE: 2.0,
+        Type.BUG: 2.0,
+        Type.STEEL: 2.0,
+        Type.FIRE: 0.5,
+        Type.WATER: 0.5,
+        Type.ROCK: 0.5,
+        Type.DRAGON: 0.5,
+    },
+    Type.WATER: {
+        Type.FIRE: 2.0,
+        Type.GROUND: 2.0,
+        Type.ROCK: 2.0,
+        Type.WATER: 0.5,
+        Type.GRASS: 0.5,
+        Type.DRAGON: 0.5,
+    },
+    Type.ELECTRIC: {
+        Type.WATER: 2.0,
+        Type.FLYING: 2.0,
+        Type.GROUND: 0.0,
+        Type.GRASS: 0.5,
+        Type.ELECTRIC: 0.5,
+        Type.DRAGON: 0.5,
+    },
+    Type.GRASS: {
+        Type.WATER: 2.0,
+        Type.GROUND: 2.0,
+        Type.ROCK: 2.0,
+        Type.FIRE: 0.5,
+        Type.GRASS: 0.5,
+        Type.POISON: 0.5,
+        Type.FLYING: 0.5,
+        Type.BUG: 0.5,
+        Type.DRAGON: 0.5,
+        Type.STEEL: 0.5,
+    },
+    Type.ICE: {
+        Type.GRASS: 2.0,
+        Type.GROUND: 2.0,
+        Type.FLYING: 2.0,
+        Type.DRAGON: 2.0,
+        Type.FIRE: 0.5,
+        Type.WATER: 0.5,
+        Type.ICE: 0.5,
+        Type.STEEL: 0.5,
+    },
+    Type.FIGHTING: {
+        Type.NORMAL: 2.0,
+        Type.ICE: 2.0,
+        Type.ROCK: 2.0,
+        Type.DARK: 2.0,
+        Type.STEEL: 2.0,
+        Type.POISON: 0.5,
+        Type.FLYING: 0.5,
+        Type.PSYCHIC: 0.5,
+        Type.BUG: 0.5,
+        Type.FAIRY: 0.5,
+        Type.GHOST: 0.0,
+    },
+    Type.POISON: {
+        Type.GRASS: 2.0,
+        Type.FAIRY: 2.0,
+        Type.POISON: 0.5,
+        Type.GROUND: 0.5,
+        Type.ROCK: 0.5,
+        Type.GHOST: 0.5,
+        Type.STEEL: 0.0,
+    },
+    Type.GROUND: {
+        Type.FIRE: 2.0,
+        Type.ELECTRIC: 2.0,
+        Type.POISON: 2.0,
+        Type.ROCK: 2.0,
+        Type.STEEL: 2.0,
+        Type.GRASS: 0.5,
+        Type.BUG: 0.5,
+        Type.FLYING: 0.0,
+    },
+    Type.FLYING: {
+        Type.GRASS: 2.0,
+        Type.FIGHTING: 2.0,
+        Type.BUG: 2.0,
+        Type.ELECTRIC: 0.5,
+        Type.ROCK: 0.5,
+        Type.STEEL: 0.5,
+    },
+    Type.PSYCHIC: {
+        Type.FIGHTING: 2.0,
+        Type.POISON: 2.0,
+        Type.PSYCHIC: 0.5,
+        Type.STEEL: 0.5,
+        Type.DARK: 0.0,
+    },
+    Type.BUG: {
+        Type.GRASS: 2.0,
+        Type.PSYCHIC: 2.0,
+        Type.DARK: 2.0,
+        Type.FIRE: 0.5,
+        Type.FIGHTING: 0.5,
+        Type.POISON: 0.5,
+        Type.FLYING: 0.5,
+        Type.GHOST: 0.5,
+        Type.STEEL: 0.5,
+        Type.FAIRY: 0.5,
+    },
+    Type.ROCK: {
+        Type.FIRE: 2.0,
+        Type.ICE: 2.0,
+        Type.FLYING: 2.0,
+        Type.BUG: 2.0,
+        Type.FIGHTING: 0.5,
+        Type.GROUND: 0.5,
+        Type.STEEL: 0.5,
+    },
+    Type.GHOST: {
+        Type.PSYCHIC: 2.0,
+        Type.GHOST: 2.0,
+        Type.DARK: 0.5,
+        Type.NORMAL: 0.0,
+    },
+    Type.DRAGON: {
+        Type.DRAGON: 2.0,
+        Type.STEEL: 0.5,
+        Type.FAIRY: 0.0,
+    },
+    Type.DARK: {
+        Type.PSYCHIC: 2.0,
+        Type.GHOST: 2.0,
+        Type.FIGHTING: 0.5,
+        Type.DARK: 0.5,
+        Type.FAIRY: 0.5,
+    },
+    Type.STEEL: {
+        Type.ICE: 2.0,
+        Type.ROCK: 2.0,
+        Type.FAIRY: 2.0,
+        Type.FIRE: 0.5,
+        Type.WATER: 0.5,
+        Type.ELECTRIC: 0.5,
+        Type.STEEL: 0.5,
+    },
+    Type.FAIRY: {
+        Type.FIGHTING: 2.0,
+        Type.DRAGON: 2.0,
+        Type.DARK: 2.0,
+        Type.FIRE: 0.5,
+        Type.POISON: 0.5,
+        Type.STEEL: 0.5,
+    },
+}
--- tests/test_types.py
+++ tests/test_types.py
@@ -24,3 +24,23 @@ def test_type_values():
 def test_type_comparison():
     assert Type.FIRE != Type.WATER
     assert Type.FIRE == Type.FIRE
+
+def test_super_effective():
+    assert Type.get_effectiveness(Type.WATER, Type.FIRE) == 2.0
+    assert Type.get_effectiveness(Type.GROUND, Type.ELECTRIC) == 2.0
+    assert Type.get_effectiveness(Type.FIGHTING, Type.NORMAL) == 2.0
+
+def test_not_very_effective():
+    assert Type.get_effectiveness(Type.FIRE, Type.WATER) == 0.5
+    assert Type.get_effectiveness(Type.GRASS, Type.FIRE) == 0.5
+    assert Type.get_effectiveness(Type.NORMAL, Type.ROCK) == 0.5
+
+def test_no_effect():
+    assert Type.get_effectiveness(Type.NORMAL, Type.GHOST) == 0.0
+    assert Type.get_effectiveness(Type.ELECTRIC, Type.GROUND) == 0.0
+    assert Type.get_effectiveness(Type.FIGHTING, Type.GHOST) == 0.0
+
+def test_normal_effectiveness():
+    assert Type.get_effectiveness(Type.FIRE, Type.PSYCHIC) == 1.0
+    assert Type.get_effectiveness(Type.WATER, Type.FAIRY) == 1.0
+    assert Type.get_effectiveness(Type.NORMAL, Type.GRASS) == 1.0
